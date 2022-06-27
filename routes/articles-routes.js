cddc41b098dda9e7f2fa383573ea9df38edd4945
Faez-B <faezbhatti@gmail.com>
2022-06-27 16:01:13 +0200

    Ajout de la route create pour l'ajout d'un Article

--- routes/articles-routes.js
+++ routes/articles-routes.js
@@ -1,3 +1,8 @@
 const express = require('express');
 const JOI = require('joi');
 const router = express.Router();
+const articleController = require('../controllers/articles-controller');
+
+router.get('/create', articleController.create);
+
+module.exports = router;
