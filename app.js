e618fba5cdd3b9570143be1d693e0e05e1d0125d
TOUFIK-Taha98 <toufik.taha1998@gmail.com>
2022-06-27 15:23:45 +0200

    Connexion à MongoDB - OK

--- app.js
+++ app.js
@@ -3,22 +3,30 @@ const bodyParser = require("body-parser")
 const cors = require('cors')
 const app = express();
 require('dotenv').config();
+const mongoose = require('mongoose');
 const usersRoutes = require('./routes/users-routes');
 
 const port = process.env.PORT;
 app.use(bodyParser.json())
 app.use(cors());
 
-
 app.use('/api/users', usersRoutes) 
 
-
 if(process.env.NODE_ENV !== "test"){
-    app.listen(port, () => {
-        console.log('Listenning... '+port)
+    // Essayer de se connecter à la BD MONGO
+    mongoose
+    .connect(process.env.URL_MONGO)
+    .then(() => {
+        // Si c'est bon on lance le serveur
+        console.log('Connected to database');
+        app.listen(port, () => {
+            console.log('Listenning... '+port);
+        });
+    })
+    .catch(err => {
+        console.log(err)
     });
 }
-
 
 module.exports= {app};
 
