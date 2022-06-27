e5bacc39230b39e9f4ead0cc2cedcb99e438f880
Faez-B <faezbhatti@gmail.com>
2022-06-27 16:57:18 +0200

    Avancement des tests sur la fonction create des articles

--- __tests__/article.test.js
+++ __tests__/article.test.js
@@ -1,2 +1,35 @@
 const { app } = require("../app");
 const request = require("supertest");
+
+describe("CRUD Articles", () => {
+
+    // Ne pas créer un nouvel article
+    it.each([
+            {  }, // Aucun des 2 paramètres requis
+            { title : "" }, // 1 seul paramètre requis mais vide
+            { title : 10 }, // 1 seul paramètre requis mais n'est pas une string 
+            { title : "test title" }, // 1 seul paramètre requis
+            { title : "test title", content : "" }, // 2 paramètres requis mais un vide
+            { title : "test title", content : 234567 }, // 2 paramètres requis mais l'un n'est pas une string
+        ])(
+        "POST /create devrait refuser %p sans l'insérer.",
+        async (invalidObject) => {
+            const result = await request(app)
+                .post("/api/articles/create")
+                .send(invalidObject)
+                .expect(400);
+        }
+    );
+
+    // Créer un nouvel article
+    it.each([
+        { title : "test title", content : "test content" },
+    ])(
+    "POST /create devrait insérer %p. dans les articles.",
+    async () => {
+        const result = await request(app)
+            .post("/api/articles/create")
+            .expect(201);
+    }
+);
+})
