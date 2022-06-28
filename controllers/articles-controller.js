e99c6f451ccd291b3ad8666ac2dd0eb25f7b2173
Faez-B <faezbhatti@gmail.com>
2022-06-28 10:36:14 +0200

    Implémentation de la méthode update d'un Article

--- controllers/articles-controller.js
+++ controllers/articles-controller.js
@@ -18,7 +18,14 @@ const create = async (req, res) => {
 }
 
 const update = async (req, res) => {
-    
+    const payload = req.body;
+    const { error } = joiSchema.validate(payload);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const article = await Article.findByIdAndUpdate(req.params.id, payload);
+    if (!article) return res.status(404).send("L'article n'existe pas.");
+
+    res.status(200).send({ _id : article._id, ...payload});
 }
 
 const deleteArticle = (req, res) => {
