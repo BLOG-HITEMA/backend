bc1e7aabadd477cebea9c035339cd8268ce2e7c8
Faez-B <faezbhatti@gmail.com>
2022-06-28 14:10:46 +0200

    Utilisation de la méthode sendStatus() au lieu de status()

--- controllers/articles-controller.js
+++ controllers/articles-controller.js
@@ -3,7 +3,7 @@ const { Article, joiSchema } = require('../models/article');
 const create = async (req, res) => {
     const payload = req.body;
     const { error } = joiSchema.validate(payload);
-    if (error) return res.status(400).send(error.details[0].message);
+    if (error) return res.sendStatus(400).send(error.details[0].message);
 
     const article = new Article({
         title: payload.title,
@@ -14,25 +14,26 @@ const create = async (req, res) => {
     });
 
     await article.save();
-    res.status(201).send(article);
+    res.sendStatus(201).send(article);
+    res.sendStatus(400).send({erreur : error.message});
 }
 
 const update = async (req, res) => {
     const payload = req.body;
     const { error } = joiSchema.validate(payload);
-    if (error) return res.status(400).send(error.details[0].message);
+    if (error) return res.sendStatus(400).send(error.details[0].message);
 
     const article = await Article.findByIdAndUpdate(req.params.id, payload);
-    if (!article) return res.status(404).send("L'article n'existe pas.");
+    if (!article) return res.sendStatus(404).send("L'article n'existe pas.");
 
-    res.status(200).send({ _id : article._id, ...payload});
+    res.sendStatus(200).send({ _id : article._id, ...payload});
 }
 
 const deleteArticle = async (req, res) => {
     const article = await Article.findByIdAndDelete(req.params.id).exec()
     .then( (data) => {
-        if (!data) return res.status(404).send({message : "L'article n'existe pas."});
-        res.status(200).send("Article supprimé.");
+        if (!data) return res.sendStatus(404).send({message : "L'article n'existe pas."});
+        res.sendStatus(200).send("Article supprimé.");
     });
 }
 
