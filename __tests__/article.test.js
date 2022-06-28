43be64a1d71ec05d14df9822d5a83aa78e486b7c
Faez-B <faezbhatti@gmail.com>
2022-06-28 11:10:24 +0200

    Factorisation des cas de tests des Articles

--- __tests__/article.test.js
+++ __tests__/article.test.js
@@ -1,17 +1,30 @@
 const { app } = require("../app");
 const request = require("supertest");
 
-describe("CRUD Articles", () => {
+const articlesValid = [
+    { title : "test title", content : "test content" }, 
+    { title : "test title", content : "test content", image : "image.png" },
+    { title : "test title", content : "test content", image : "" },
+    { title : "test title", content : "test content", image : "image.png", published : true },
+    { title : "test title", content : "test content", image : "image.png", published : false },
+    { title : "test title", content : "test content", image : "image.png", published : false, message : "test message" },
+    { title : "test title", content : "test content", image : "image.png", published : true, message : "test message" },
+    { title : "test title", content : "test content", image : "image.png", published : true, message : "" },
+    { title : "test title", content : "test content", image : "image.png", published : false, message : "" }
+];
 
+const articlesInvalid = [
+    {  }, // Aucun des 2 paramètres requis
+    { title : "" }, // 1 seul paramètre requis mais vide
+    { title : 10 }, // 1 seul paramètre requis mais n'est pas une string 
+    { title : "test title" }, // 1 seul paramètre requis
+    { title : "test title", content : "" }, // 2 paramètres requis mais un vide
+    { title : "test title", content : 234567 }, // 2 paramètres requis mais l'un n'est pas une string
+];
+
+describe("CRUD Articles", () => {
     // Ne pas créer un nouvel article
-    it.each([
-            {  }, // Aucun des 2 paramètres requis
-            { title : "" }, // 1 seul paramètre requis mais vide
-            { title : 10 }, // 1 seul paramètre requis mais n'est pas une string 
-            { title : "test title" }, // 1 seul paramètre requis
-            { title : "test title", content : "" }, // 2 paramètres requis mais un vide
-            { title : "test title", content : 234567 }, // 2 paramètres requis mais l'un n'est pas une string
-        ])(
+    it.each(articlesInvalid)(
         "POST /create devrait refuser %p sans l'insérer.",
         async (invalidObject) => {
             const result = await request(app)
@@ -22,17 +35,7 @@ describe("CRUD Articles", () => {
     );
 
     // Créer un nouvel article
-    it.each([
-        { title : "test title", content : "test content" }, 
-        { title : "test title", content : "test content", image : "image.png" },
-        { title : "test title", content : "test content", image : "" },
-        { title : "test title", content : "test content", image : "image.png", published : true },
-        { title : "test title", content : "test content", image : "image.png", published : false },
-        { title : "test title", content : "test content", image : "image.png", published : false, message : "test message" },
-        { title : "test title", content : "test content", image : "image.png", published : true, message : "test message" },
-        { title : "test title", content : "test content", image : "image.png", published : true, message : "" },
-        { title : "test title", content : "test content", image : "image.png", published : false, message : "" },
-    ])(
+    it.each(articlesValid)(
         "POST /create devrait insérer %p dans les articles.",
         
         (article) => {
